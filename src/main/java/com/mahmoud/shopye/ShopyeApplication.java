418d440816092fcf71ab563b026f7ae11be08d13
Mahmoud Reda <mahmoudreda027@gmail.com>
2024-04-21 20:32:14 +0200

    initial shopye

---
+++ src/main/java/com/mahmoud/shopye/ShopyeApplication.java
@@ -0,0 +1,13 @@
+package com.mahmoud.shopye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopyeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mahmoud/shopye/ShopyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mahmoud.shopye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopyeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
